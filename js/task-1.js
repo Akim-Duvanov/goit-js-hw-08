7dcc4d0fbf552a543bdeb7c2554a3bb0180f0d1a
Akim <akimduvanov@gmail.com>
2025-05-20 22:09:30 +0200

    dz8 js

--- js/task-1.js
+++
@@ -1,6 +0,0 @@
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categories.length}`);
-for (const elem of categories) {
-    console.log(`Category: ${elem.querySelector("h2").textContent}`);
-    console.log(`Elements: ${elem.querySelectorAll("li").length}`);
-}
